782535cbc2a8cfbd698cce916e5a497496bf3df2
samuelwildary <samuelwildary@gmail.com>
2025-10-25 19:55:11 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Backend is running successfully"}
+
+from app.database import engine
+from app import models
+
+@app.on_event("startup")
+def create_tables():
+    print("🔄 Criando tabelas no banco se não existirem...")
+    models.Base.metadata.create_all(bind=engine)
+    print("✅ Tabelas criadas com sucesso!")
